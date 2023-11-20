bf07f7e790a20a27fa398e3ac3010c8128a039cf
Pragadeek <150142041+Pragadeek@users.noreply.github.com>
2023-11-20 19:22:17 +0530

    Initial Commit

--- lib/src/test/java/demoproject/LibraryTest.java
+++ lib/src/test/java/demoproject/LibraryTest.java
@@ -10,6 +10,6 @@ class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
         Library classUnderTest = new Library();
         
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+       assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
     }
 }
